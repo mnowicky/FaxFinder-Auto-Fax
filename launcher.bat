0742ff2bb9a5a3715ad339aab3f1424bdde0d4cc
mnowicky <56040330+mnowicky@users.noreply.github.com>
2022-06-29 11:57:14 -0400

    Update launcher.bat

--- launcher.bat
+++ launcher.bat
@@ -1,2 +1,5 @@
-cd C:\InHouseSoftware\AutoFaxer
+:: remove REM and change dir to folder containg AutoFaxer.py.
+:: this simply exists to be used with a scheduled task to start/re-start the program. 
+
+REM cd C:\InHouseSoftware\AutoFaxer
 "C:\Program Files\Python36\Python.exe" AutoFaxer.py
